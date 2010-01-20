1bf268686892db0d2dd2d3bb70989d1a6b76b0c7
John <john@theworkinggroup.ca>
2010-01-20 23:24:03 +0000

    init

---
+++ external_wt.js
@@ -0,0 +1,15 @@
+﻿function genWtValues() {
+    //simplified webtrends cookie reading & invoice values, etc
+
+    //find cookie trail
+    if (Cookie.get('trail_txt')!==null) {       
+        wt = Cookie.get('trail_txt'); //read in cookie
+        Cookie.erase('trail_txt');
+    }
+
+    //build invoice number, date & time
+    var tmpDate = new Date();
+    wtInv = Math.floor(Math.random()*1000000000);
+    wtDate = tmpDate.getMonth() + ':' + tmpDate.getDate() + ':' + tmpDate.getFullYear();
+    wtTime = tmpDate.getHours() + ':' + tmpDate.getMinutes() + ':' + tmpDate.getSeconds();
+}
